bf1996eee99e9cb00cc57b4df06ffbfed8f1ac04
Aniruddh Fichadia <Ani.Fichadia@gmail.com>
2021-02-14 21:53:30 +1100

    Sets up basic routing

--- server/src/com/example/Server.kt
+++ server/src/com/example/Server.kt
@@ -2,18 +2,35 @@ package com.example
 
 import io.ktor.application.Application
 import io.ktor.application.ApplicationCall
+import io.ktor.application.call
+import io.ktor.application.feature
 import io.ktor.application.install
 import io.ktor.application.log
 import io.ktor.features.CallLogging
 import io.ktor.features.DefaultHeaders
 import io.ktor.features.DoubleReceive
+import io.ktor.features.StatusPages
+import io.ktor.http.ContentType
+import io.ktor.http.HttpStatusCode
 import io.ktor.request.ApplicationRequest
 import io.ktor.request.httpMethod
 import io.ktor.request.uri
 import io.ktor.response.ApplicationResponse
+import io.ktor.response.respond
+import io.ktor.response.respondText
+import io.ktor.routing.HttpMethodRouteSelector
+import io.ktor.routing.Route
+import io.ktor.routing.Routing
+import io.ktor.routing.get
+import io.ktor.routing.post
+import io.ktor.routing.route
+import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import io.ktor.util.error
 import org.slf4j.event.Level
+import kotlin.random.Random
+import kotlin.random.nextInt
 
 object Server {
     @JvmStatic
@@ -48,4 +65,72 @@ fun Application.module(testing: Boolean = false) {
             "$uri ($method, body: $requestHasBody) -> $responseStatus"
         }
     }
+    install(StatusPages) {
+        exception<Throwable> { cause ->
+            log.error(cause)
+
+            call.respond(HttpStatusCode.InternalServerError)
+        }
+    }
+    install(Routing)
+
+    routing {
+        // TODO: if you need to debug routing, enabling this logs the routing on every call
+        if (testing) {
+            trace { log.debug(it.buildText()) }
+        }
+
+        route("/") {
+            get {
+                val request: ApplicationRequest = call.request
+                // TODO: query param access
+                val name = request.queryParameters["name"]
+
+                val text = if (name != null) {
+                    "Hello $name!"
+                } else {
+                    "Hello world!"
+                }
+
+                call.respondText(text, contentType = ContentType.Text.Plain)
+            }
+
+            post("/") {
+                // TODO: responding with error status codes
+                call.respond(HttpStatusCode.InternalServerError)
+            }
+        }
+
+
+        get("/random") {
+            val from = try {
+                call.request.queryParameters["from"]?.toInt() ?: 0
+            } catch (exception: Throwable) {
+                return@get call.respond(HttpStatusCode.BadRequest)
+            }
+            val to = try {
+                call.request.queryParameters["to"]?.toInt() ?: Int.MAX_VALUE
+            } catch (exception: Throwable) {
+                return@get call.respond(HttpStatusCode.BadRequest)
+            }
+
+            val randomValue = Random.nextInt(from..to)
+
+            call.respondText(randomValue.toString(), contentType = ContentType.Text.Plain)
+        }
+    }
+
+    // TODO: If you need to log all routes, you can use this
+    if (testing) {
+        val root = feature(Routing)
+        val allRoutes = allRoutes(root)
+        val allRoutesWithMethod = allRoutes.filter { it.selector is HttpMethodRouteSelector }
+        allRoutesWithMethod.forEach {
+            log.info("route: $it")
+        }
+    }
+}
+
+fun allRoutes(root: Route): List<Route> {
+    return listOf(root) + root.children.flatMap { allRoutes(it) }
 }
--- server/test/com/example/ServerTest.kt
+++ server/test/com/example/ServerTest.kt
@@ -1,5 +1,58 @@
 package com.example
 
+import io.ktor.http.ContentType
+import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpMethod
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.testing.handleRequest
+import io.ktor.server.testing.withTestApplication
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
 class ServerTest {
-    
+
+    @Test
+    fun testRoot() {
+        withTestApplication({ module(testing = true) }) {
+            handleRequest(HttpMethod.Get, "/").apply {
+                assertEquals(HttpStatusCode.OK, response.status())
+                assertEquals("Hello world!", response.content)
+            }
+
+            handleRequest(HttpMethod.Get, "/?name=ani").apply {
+                assertEquals(HttpStatusCode.OK, response.status())
+                assertEquals("Hello ani!", response.content)
+            }
+
+            handleRequest(HttpMethod.Post, "/").apply {
+                assertEquals(HttpStatusCode.InternalServerError, response.status())
+            }
+        }
+    }
+
+    @Test
+    fun testRandom() {
+        withTestApplication({ module(testing = true) }) {
+            handleRequest(HttpMethod.Get, "/random").apply {
+                assertEquals(HttpStatusCode.OK, response.status())
+
+                println(response.content)
+                val contentInt = response.content!!.toInt()
+                assertTrue { contentInt in 0..Int.MAX_VALUE }
+            }
+
+
+            handleRequest(HttpMethod.Get, "/random?from=notAnInt").apply {
+                assertEquals(HttpStatusCode.BadRequest, response.status())
+            }
+
+            handleRequest(HttpMethod.Get, "/random?from=10&to=100").apply {
+                assertEquals(HttpStatusCode.OK, response.status())
+
+                val contentInt = response.content!!.toInt()
+                assertTrue { contentInt in 10..100 }
+            }
+        }
+    }
 }
